3154724a18edb88f1eea21195c38954e3159079a
test2002maker <22546650@qq.com>
2020-09-07 21:02:21 +0800

    -mh

---
+++ zuoye/王宇生9.7/9.7zuoye.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-  
+#====#====#====#====   
+#Author:
+#CreatDate:
+#Version: 
+#====#====#====#====
+
+# 3.请在电商网站上找10个元素,用id和name方式获取,打印元素除id和name外的其他属性,提交到git上
+# from selenium import webdriver
+# driver = webdriver.Firefox()
+# driver.get('http://39.96.181.61/qftest-ds/user/register.html')
+# print(driver.find_element_by_id('password').get_attribute('name'))
+# print(driver.find_element_by_name('password').get_attribute('id'))
+# print(driver.find_element_by_id('password').get_attribute('type'))
+# print(driver.find_element_by_id('password').get_attribute('data-warnpos'))
+# print(driver.find_element_by_id('agree').get_attribute('name'))
+# print(driver.find_element_by_id('agree').get_attribute('type'))
+# print(driver.find_element_by_id('agree').get_attribute('checked'))
+# print(driver.find_element_by_id('email').get_attribute('type'))
+# print(driver.find_element_by_id('username').get_attribute('type'))
+# print(driver.find_element_by_id('email').get_attribute('type'))
+# driver.quit()
+
+# 4.电商网站上找5个超链接元素,通过超链接文字获取元素,打印链接地址,提交到git上
+# from selenium import webdriver
+# driver = webdriver.Firefox()
+# driver.get('http://39.96.181.61/qftest-ds/index.html')
+# print(driver.find_element_by_link_text('臭豆腐').get_attribute('href'))
+# print(driver.find_element_by_link_text('免费注册').get_attribute('href'))
+# print(driver.find_element_by_link_text('烤鸭').get_attribute('href'))
+# print(driver.find_element_by_link_text('烧鹅').get_attribute('href'))
+# print(driver.find_element_by_link_text('小白').get_attribute('href'))
+# driver.quit()
